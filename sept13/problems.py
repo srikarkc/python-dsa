7900aedab71b903b73f3372832f2b9a6037d65f3
Srikar KC <34884850+srikarkc@users.noreply.github.com>
2025-09-13 22:27:53 -0700

    Day 8 - solve the linked list problems again

--- sept13/problems.py
+++ sept13/problems.py
@@ -151,3 +151,128 @@ class LinkedList:
             slow = slow.next
             fast = fast.next.next
         return slow
+    
+    def has_loop(self):
+        slow, fast = self.head, self.head
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+            if slow is fast:
+                return True
+            
+        return False
+    
+    def find_kth_from_end(self, k):
+        slow, fast = self.head, self.head
+
+        for _ in range(k):
+            fast = fast.next
+
+        while fast is not None:
+            slow = slow.next
+            fast = fast.next
+
+        return slow
+    
+
+    # Problem 4 - remove duplicates
+    def remove_duplicates(self):
+        seen_values = set()
+
+        prev = None
+        curr = self.head
+
+        while curr is not None:
+            if curr.value in seen_values:
+                prev.next = curr.next
+                if curr is self.tail:
+                    self.tail = prev
+                nxt = curr.next
+                curr.next = None
+                self.lenght -= 1
+                self.curr = nxt
+            else:
+                seen_values.add(curr.value)
+                curr = curr.next
+                prev = prev.next
+
+    # Problem 5 - Binary to decimal
+    def binary_to_decimal(self):
+        if not self.head:
+            return
+        
+        decimal = 0
+        temp = self.head
+        
+        while temp is not None:
+            decimal = (2 * decimal) + (temp.value)
+            temp = temp.next
+
+        return decimal
+    
+    # Problem 6 - Partition list based on a give value
+    def partition_list(self, value):
+        dummy1, dummy2 = Node(0), Node(0)
+        p1, p2 = dummy1, dummy2
+
+        temp = self.head
+
+        while temp is not None:
+            if temp.value < value:
+                p1.next = temp
+                p1 = p1.next
+            elif temp.value >= value:
+                p2.next = temp
+                p2 = p2.next
+            temp = temp.next
+
+        p1.next = dummy2.next
+        p2.next = None
+        self.head = dummy1.next
+
+        # reset tail
+        self.tail = p1 if dummy2.next is None else p2
+
+    def reverse_between(self,left,right):
+        if self.length <= 1:
+            return
+        
+        dummy = Node(0)
+        dummy.next = self.head
+        pre = dummy
+
+        for i in range(left):
+            pre = pre.next
+
+        curr = pre.next
+
+        for i in range(right - left):
+            node_to_move = curr.next
+            curr.next = node_to_move.next
+            node_to_move.next = pre.next
+            pre.next = node_to_move
+
+        self.head = dummy.next
+
+    # Problem 8 - swap pairs
+    def swap_pairs(self):
+        if not self.head or not self.head.next:
+            return
+        
+        dummy = Node(0)
+        dummy.next = self.head
+        prev = dummy
+        
+        while prev.next and prev.next.next:
+            first = prev.next
+            second = prev.next.next
+
+            # Swap
+            first.next = second.next
+            second.next = first
+            prev.next = second
+
+            # Move prev to the next pair
+            prev = first
+
+        self.head = dummy.next
