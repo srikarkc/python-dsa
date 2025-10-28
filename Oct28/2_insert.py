22899af066d557a2ef71d2564e9fa7a13d8b7f1a
Srikar K <sclifeisgreat@gmail.com>
2025-10-28 08:12:51 -0700

    Day 51 - recursive

---
+++ Oct28/2_insert.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        
+def __r_insert(self, current_node, value):
+    if current_node == None:
+        return Node(value)
+    if value < current_node.value:
+        current_node.left = self.__r_insert(current_node.left, value)
+    if value > current_node.value:
+        current_node.right = self.__r_insert(current_node.right, value)
+    return current_node
+    
+def r_insert(self, value):
+    if self.root == None:
+        self.root = Node(value)
+    self.__r_insert(self.root, value)
