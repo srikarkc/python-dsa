f652196573a96fd88720e4e88be5d9022bd01527
Srikar K <sclifeisgreat@gmail.com>
2025-11-10 16:57:38 -0800

    Nov 10 - Trie DS

---
+++ Nov10/02_add_search_word.py
@@ -0,0 +1,36 @@
+# Not actually going to solve the problem here
+
+# Going to just learn about Trie
+
+# https://neetcode.io/problems/design-word-search-data-structure?list=neetcode150
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end_of_word = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        node = self.root
+        for char in word:
+            node = node.children.setdefault(char, TrieNode())
+        node.is_end_of_word = True
+
+    def search(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.is_end_of_word
+    
+    def starts_with(self, prefix):
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
