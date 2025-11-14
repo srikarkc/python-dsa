e4394854ebffc7a8f5f55b2910cf8f7b691d564a
Srikar K <sclifeisgreat@gmail.com>
2025-11-14 12:57:20 -0800

    Nov 14 - container with most water

---
+++ Nov14/01_containers_most_water.py
@@ -0,0 +1,14 @@
+def maxArea(heights):
+    l, r = 0, len(heights) - 1
+    res = 0
+
+    while l < r:
+        area = min(heights[l], heights[r]) * (r - l)
+        res = max(res, area)
+
+        if heights[l] <= heights[r]:
+            l += 1
+        else:
+            r -= 1
+
+    return res
