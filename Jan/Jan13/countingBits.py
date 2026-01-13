e1e601944b92406f447f44a5ff286c4f234ae1e9
Srikar K <sclifeisgreat@gmail.com>
2026-01-13 08:22:18 -0800

    +countBits problem

---
+++ Jan/Jan13/countingBits.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countBits(self, n):
+        ans = [0] * (n + 1)
+        offset = 1
+
+        for i in range(1, n + 1):
+            if i == offset * 2:
+                offset = i
+            ans[i] = 1 + ans[i - offset]
+
+        return ans
