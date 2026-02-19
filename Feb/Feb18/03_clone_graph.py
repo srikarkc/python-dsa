9d3b66a5688e86a26bc2783bb1c6ff49b4ed0ba7
Srikar K <sclifeisgreat@gmail.com>
2026-02-18 22:46:52 -0800

    fix

--- Feb/Feb18/03_clone_graph.py
+++ Feb/Feb18/03_clone_graph.py
@@ -1,9 +1,10 @@
 from collections import deque
 
 class Node:
-    def __init__(self, val=0, left=None, right=None):
+    def __init__(self, val = 0, neighbors = None):
         self.val = val
-        self.left, self.right = left, right
+        self.neighbors = neighbors if neighbors is not None else []
+
 
 class Solution:
     def cloneGraph(self, node):
