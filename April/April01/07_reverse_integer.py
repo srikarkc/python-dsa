0f6c032b369fcd169c823e7a9fff284d087d7b49
Srikar KC <34884850+srikarkc@users.noreply.github.com>
2026-04-01 18:57:36 -0700

    April01 commit

---
+++ April/April01/07_reverse_integer.py
@@ -0,0 +1,17 @@
+class Solution:
+    def reverseInteger(self, x):
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+        
+        res = 0
+
+        while x != 0:
+            digit = x % 10
+
+            if res > 214748364 or (res == 214748364 and digit > 7):
+                return 0
+
+            res = res * 10 + digit
+            x //= 10
+
+        return sign * res
