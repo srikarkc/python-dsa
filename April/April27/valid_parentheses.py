30e6e08f8eecbb0a687410912cea00154629ada5
Srikar K <sclifeisgreat@gmail.com>
2026-04-27 18:22:43 -0700

    April27 commit

---
+++ April/April27/valid_parentheses.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s):
+        stack = []
+        match = {
+            '}': '{',
+            ']': '[',
+            ')': '('
+        }
+
+        for ch in s:
+            if ch in match:
+                if not stack or stack[-1] != match[ch]:
+                    return False
+                stack.pop()
+            else:
+                stack.append(ch)
+
+        return len(stack) == 0
+    
