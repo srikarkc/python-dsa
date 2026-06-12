437e24e87c54a1f80b8de43d749ded4758e6cf5a
Srikar KC <34884850+srikarkc@users.noreply.github.com>
2026-06-11 20:30:34 -0700

    June11 commit

---
+++ June/June11/merge_k_sorted_ll.py
@@ -0,0 +1,27 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val, self.next = val, next
+
+import heapq
+
+class Solution:
+    def mergeKLists(self, lists):
+        heap = []
+
+        for i, node in enumerate(lists):
+            if node:
+                heapq.heappush(heap, (node.val, i, node))
+
+        dummy = ListNode()
+        curr = dummy
+
+        while heap:
+            val, i, node = heapq.heappop(heap)
+            curr.next = node
+            curr = curr.next
+
+            if node.next:
+                heapq.heappush(heap, (node.next.val, i, node.next))
+
+        return dummy.next
+    
