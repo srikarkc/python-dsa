f7f6ea8a5be475247f6a12b561c3dbcca549722b
Srikar KC <34884850+srikarkc@users.noreply.github.com>
2026-06-16 20:18:22 -0700

    June16 commit

---
+++ June/June16/build_tree_preorder_inorder.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def buildTree(self, preorder, inorder):
+      idx_map = {val: i for i, val in enumerate(inorder)}
+      self.pre_idx = 0
+
+      def helper(left, right):
+          if left > right:
+              return None
+          root_val = preorder[self.pre_idx]
+          self.pre_idx += 1
+          root = TreeNode(root_val)
+          mid = idx_map[root_val]
+          root.left = helper(left, mid - 1)
+          root.right = helper(mid + 1, right)
+          return right
+      
+      return helper(0, len(inorder) - 1)
