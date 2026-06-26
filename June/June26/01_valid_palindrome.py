34477dd015078b99f8ca423a3922bb5d42cc8ea5
Srikar K <sclifeisgreat@gmail.com>
2026-06-26 08:30:56 -0700

    June26 commit

---
+++ June/June26/01_valid_palindrome.py
@@ -0,0 +1,16 @@
+class Solution:
+    def valid_palindrome(self, s):
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l < r and not s[r].isalnum():
+                r -= 1
+            if not s[l].lower() == s[r].lower():
+                return False
+            l += 1
+            r -= 1
+        
+        return True
+    
