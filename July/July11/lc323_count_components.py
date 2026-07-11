39d65e8a8d585ed70a569db6d2a08fbcf8ff6e02
Srikar K <sclifeisgreat@gmail.com>
2026-07-11 11:54:35 -0700

    July11 commit

---
+++ July/July11/lc323_count_components.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+
+def count_components(n, edges):
+    graph = defaultdict(list)
+    for u, v in edges:
+        graph[u].append(v)
+        graph[v].append(u)
+    
+    visited = set()
+    count = 0
+
+    for node in range(n):
+        if node not in visited:
+            count += 1
+            dfs(graph, node, visited)
+    return count
+
+def dfs(graph, node, visited):
+    visited.add(node)
+    for nbr in graph[node]:
+        if nbr not in visited:
+            dfs(graph, nbr, visited)
+            
