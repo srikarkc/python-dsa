8d4402f731025ec94597738ab2185616dd7d590d
Srikar K <sclifeisgreat@gmail.com>
2026-07-28 23:42:54 -0700

    July28 commit

---
+++ July/July28/dijkstra.py
@@ -0,0 +1,23 @@
+import heapq
+from collections import defaultdict
+
+def dijkstra(n, edges, src):
+    graph = defaultdict(list)
+    for u, v, w in edges:
+        graph[u].append((v,w))
+
+    dist = [float('inf')] * n
+    dist[src] = 0
+    heap = [(0,src)]
+
+    while heap:
+        d, node = heapq.heappop(heap)
+        if d > dist[node]:
+            continue
+        for nbr, w in graph[node]:
+            nd = d + w
+            if nd < dist[nbr]:
+                dist[nbr] = nd
+                heapq.heappush(heap, (nd, nbr))
+
+    return dist
